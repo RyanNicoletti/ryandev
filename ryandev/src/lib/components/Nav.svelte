f4150084adca1fbb8c748b7db43109805946d41b
Ryan <r.nicoletti1@gmail.com>
2023-07-08 14:42:26 -0400

    add routes and mdsvex

---
+++ ryandev/src/lib/components/Nav.svelte
@@ -0,0 +1,23 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li><a href="/about">About</a></li>
+		<li>
+			<a href="/blog">Blog</a>
+		</li>
+	</ul>
+</nav>
+
+<style>
+	ul {
+		margin: 0;
+		list-style-type: none;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+</style>
---
+++ ryandev/svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
+	extensions: ['.svelte', '.md'],
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
